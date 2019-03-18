f5b59bb5d941c70ca96b87067d0ca0e9e1ae5bca
Saulo Oliveira <oliveira.saulo06@gmail.com>
2019-03-18 14:29:54 -0400

    adicao da frase

--- src/Components/Frase.js
+++ src/Components/Frase.js
@@ -4,15 +4,15 @@ class Frase extends Component{
     render(){
         return(
             
-                <div class="container-frase-home">
-                    <div class="conteudo-frase">
-                        <div class="col-md-12 col-xs-12 col-sm-12">
-                            <p>A sua referência em capacitação proﬁssional, com facilitadores qualiﬁcados 
-                                para desenvolver cursos, workshops e palestras para 
-                                a demanda de sua equipe.</p>
-                        </div>
+            <div class="container-frase-home">
+                <div class="conteudo-frase">
+                    <div class="col-md-12 col-xs-12 col-sm-12">
+                        <p>A sua referência em capacitação proﬁssional, com facilitadores qualiﬁcados 
+                            para desenvolver cursos, workshops e palestras para 
+                            a demanda de sua equipe.</p>
                     </div>
                 </div>
+            </div>
             
         );
     }
