fede2a4e96d733d5c0e67c6649fe4ff3f058378d
Kaio Eduardo <kaioeduardoescar@gmail.com>
2019-03-19 15:59:39 -0400

    [Img] -- ajuste de boa sorte
    Signed-off-by: Kaio Eduardo <kaioeduardoescar@gmail.com>

--- src/Components/Slide.js
+++ src/Components/Slide.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
+
+let slide1 = 'arquivos-layout/images/cumprimento.jpg'
+var style1  = {
+    background: `url(${slide1}) round`
+}
+
+let slide2 = 'arquivos-layout/images/meetup.png'
+var style2  = {
+    background: `url(${slide2}) round`
+}
+
+let slide3 = 'arquivos-layout/images/cursos.jpg'
+var style3  = {
+    background: `url(${slide3}) round`
+}
+
+let slide4 = 'arquivos-layout/images/ceu-estrelado.jpg'
+var style4  = {
+    background: `url(${slide4}) round` 
+}
 class Slide extends Component {
     render(){
+
+        const slide = [`url(${slide1})`, `url(${slide1})` ,`url(${slide1})` ];
+
         return(
             <div>
             <div id="ubea-hero" class="js-fullheight"  data-section="home">
                 <div class="flexslider js-fullheight">
                     <ul class="slides">
-                        <li class="slide1">
+                        <li class="slide1" style={ style1 }>
                             <div class="overlay"></div>
                             <div class="container">
                                 <div class="col-md-10 col-md-offset-1 js-fullheight slider-text">
@@ -20,7 +43,7 @@ class Slide extends Component {
                                 </div>
                             </div>
                         </li>
-                        <li class="slide2">
+                        <li class="slide2" style={ style2 }>
                             <div class="overlay"></div>
                             <div class="container">
                                 <div class="col-md-10 col-md-offset-1 js-fullheight slider-text">
@@ -33,7 +56,7 @@ class Slide extends Component {
                                 </div>
                             </div>
                         </li>
-                        <li class="slide3">
+                        <li class="slide3"  style={ style3 } >
                             <div class="overlay"></div>
                             <div class="container">
                                 <div class="col-md-10 col-md-offset-1 js-fullheight slider-text">
@@ -46,7 +69,7 @@ class Slide extends Component {
                                 </div>
                             </div>
                         </li>
-                        <li class="slide4">
+                        <li class="slide4"  style={ style4 }>
                             <div class="overlay"></div>
                             <div class="container">
                                 <div class="col-md-10 col-md-offset-1 text-center js-fullheight slider-text">
