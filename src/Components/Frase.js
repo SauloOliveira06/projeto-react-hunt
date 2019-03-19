0f610db2e6f30e583c6ec3bc5497488107b783ae
Saulo Oliveira <oliveira.saulo06@gmail.com>
2019-03-18 21:09:43 -0400

    Alteracao de componente com sucesso

--- src/Components/Frase.js
+++ src/Components/Frase.js
@@ -1,19 +1,36 @@
 import React, {Component} from 'react';
+import client from './Client';
 
 class Frase extends Component{
+
+    constructor(){
+        super()
+        this.state = { frasepage: []}
+    }
+
+    componentDidMount(){
+        client.getEntries({
+            'content_type' : 'conteudoFrase'
+        }).then( (entries) => {
+            this.setState({frasepage: entries.items[0]})
+        })
+    }
+
     render(){
         return(
-            
-            <div class="container-frase-home">
-                <div class="conteudo-frase">
-                    <div class="col-md-12 col-xs-12 col-sm-12">
-                        <p>A sua referência em capacitação proﬁssional, com facilitadores qualiﬁcados 
-                            para desenvolver cursos, workshops e palestras para 
-                            a demanda de sua equipe.</p>
+            <div>
+                <div class="container-frase-home">
+                    <div class="conteudo-frase">
+                        <div class="col-md-12 col-xs-12 col-sm-12">
+                        {this.state.frasepage.length === 0 ?
+                            <div>Carregando</div>
+                            :
+                            <p>{this.state.frasepage.fields.conteudoFraseDesc}!!!</p>
+                        }
+                        </div>
                     </div>
                 </div>
             </div>
-            
         );
     }
 }
